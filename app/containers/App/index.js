2b1d13ab58c24a3febbe954aa78384a827d9ef3c
Emily Hernandez <bulle.96@windowslive.com>
2018-11-30 13:23:01 +0200

    Add Route for challenge page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,12 +12,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import ChallengePage from 'containers/ChallengePage/Loadable';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/challenge" component={ChallengePage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
