bd905baf715f057a2363742c0810e4ea98d87d5e
Sami <sami.haapanen@hotmail.com>
2018-12-12 17:38:12 +0200

    Add background image

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,17 +10,67 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { Typography } from '@material-ui/core';
-import messages from './messages';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
+import './style.css';
+
+const styles = theme => ({
+  root: {
+    width: 'inherit',
+  },
+  grid: {
+    margin: '0 auto',
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+  box: {
+    paddingTop: 0,
+    marginBottom: theme.spacing.unit * 2,
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+});
 
 /* eslint-disable react/prefer-stateless-function */
-export default class HomePage extends React.PureComponent {
+export class HomePage extends React.PureComponent {
+  state = {
+    header:
+      'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/EvVhCC0deijtsl1b4/videoblocks-binary-computer-code-background-motion-graphic-animation-computer-generated-seamless-loop_r2orby0ng_thumbnail-full01.png',
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <Typography variant="h2" gutterBottom>
-        <FormattedMessage {...messages.header} />
-      </Typography>
+      <div className={classes.root}>
+        <div className={classes.grid}>
+          <div className="challenge-banner" />
+        </div>
+      </div>
     );
   }
 }
+
+HomePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapDispatchToProps);
+
+export default compose(
+  withStyles(styles),
+  withConnect,
+)(HomePage);
