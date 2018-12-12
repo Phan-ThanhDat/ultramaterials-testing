0ad126ad2bed62dca90ab270ad89dbe9d70650ea
Emily Hernandez <bulle.96@windowslive.com>
2018-12-12 15:47:31 +0200

    Edit theme

--- app/utils/theme.js
+++ app/utils/theme.js
@@ -1,16 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
 
-export const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-  },
-  palette: {
-    primary: purple,
-    secondary: green,
-  },
-  status: {
-    danger: 'orange',
-  },
-});
+const palette = {
+  primary: { main: '#424242', contrastText: '#ffffff' },
+  secondary: { main: '#D50000', contrastText: '#ffffff' },
+};
+const themeName = 'Tundora Guardsman Red Xerus';
+const typography = { useNextVariants: true };
+const status = { danger: 'orange' };
+
+export default createMuiTheme({ palette, themeName, typography, status });
