8973c1e2537d6fbccfd30b7ca26bb5ec58d2ae02
Emily Hernandez <bulle.96@windowslive.com>
2018-12-12 14:51:28 +0200

    generate Header container

---
+++ app/containers/Header/index.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * Header
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectReducer from 'utils/injectReducer';
+import makeSelectHeader from './selectors';
+import reducer from './reducer';
+
+/* eslint-disable react/prefer-stateless-function */
+export class Header extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+Header.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  header: makeSelectHeader(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'header', reducer });
+
+export default compose(
+  withReducer,
+  withConnect,
+)(Header);
