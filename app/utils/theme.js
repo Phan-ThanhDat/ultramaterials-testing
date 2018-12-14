60841c6babfc743a02c6e30fa4b8e5bff4361971
Emily Hernandez <bulle.96@windowslive.com>
2018-12-14 11:57:30 +0200

    Remove shadows entirely

--- app/utils/theme.js
+++ app/utils/theme.js
@@ -1,11 +1,18 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const palette = {
-  primary: { main: '#424242', contrastText: '#ffffff' },
-  secondary: { main: '#D50000', contrastText: '#ffffff' },
-};
-const themeName = 'Tundora Guardsman Red Xerus';
-const typography = { useNextVariants: true };
-const status = { danger: 'orange' };
-
-export default createMuiTheme({ palette, themeName, typography, status });
+export default createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    type: 'light',
+    primary: { main: '#424242', contrastText: '#ffffff' },
+    secondary: { main: '#D50000', contrastText: '#ffffff' },
+  },
+  themeName: 'Ultrahack theme',
+  shadows: ['none'],
+  overrides: {
+    MuiPaper: {},
+    MuiAppBar: {},
+  },
+});
