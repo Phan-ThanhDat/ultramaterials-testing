4eb24798045043bfd2fe92132be8aa94821ee10c
Emily Hernandez <bulle.96@windowslive.com>
2018-12-18 12:58:59 +0200

    Add 'header' section

--- app/containers/ChallengesPage/index.js
+++ app/containers/ChallengesPage/index.js
@@ -13,13 +13,18 @@ import { withStyles } from '@material-ui/core/styles';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
+import { Typography } from '@material-ui/core';
 import makeSelectChallengesPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
 const styles = {
   root: {
-    textAlign: 'center',
+    textAlign: 'left',
+  },
+  grid: {
+    maxWidth: 900,
+    margin: '0 auto',
   },
 };
 
@@ -27,7 +32,27 @@ const styles = {
 export class ChallengesPage extends React.Component {
   render() {
     const { classes } = this.props;
-    return <div className={classes.root} />;
+    return (
+      <div className={classes.root}>
+        <div className={classes.grid}>
+          <Typography variant="h4">Challenges</Typography>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            debitis distinctio maxime! Voluptas blanditiis ipsa laudantium
+            voluptate illum omnis ullam impedit odio non tenetur, doloribus vel
+            ea, laborum provident vero corrupti sed autem rem ipsam voluptatem
+            maiores tempore qui eveniet? Accusamus dolorum natus ipsum pariatur
+            fuga incidunt eaque dignissimos aut voluptates, enim nostrum quod
+            consequatur vero unde asperiores amet delectus sit aspernatur
+            maiores rerum laboriosam quae modi tempore. Possimus magni vitae
+            animi aliquam modi. Ipsa laboriosam eum blanditiis! Suscipit
+            architecto eligendi, quos facere dignissimos dolorum quam in
+            provident ea veritatis iste dolores reprehenderit modi ipsam sunt?
+            Voluptatum architecto commodi inventore eveniet doloribus.
+          </Typography>
+        </div>
+      </div>
+    );
   }
 }
 
