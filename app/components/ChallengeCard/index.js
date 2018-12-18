81b824df15a95c98757dd4f5de1efda463cd04d2
Emily Hernandez <bulle.96@windowslive.com>
2018-12-18 12:59:37 +0200

    Create ChallengeCard component

---
+++ app/components/ChallengeCard/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ChallengeCard
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ChallengeCard() {
+  return <div />;
+}
+
+ChallengeCard.propTypes = {};
+
+export default ChallengeCard;
