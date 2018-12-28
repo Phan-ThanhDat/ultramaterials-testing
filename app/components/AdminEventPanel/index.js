626142103069ff61d8865798849942767fa41893
Emily Hernandez <bulle.96@windowslive.com>
2018-12-28 13:57:28 +0200

    Create Admin Event Panel component

---
+++ app/components/AdminEventPanel/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * AdminEventPanel
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class AdminEventPanel extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+AdminEventPanel.propTypes = {};
+
+export default AdminEventPanel;
