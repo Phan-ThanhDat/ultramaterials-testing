9f78a7e09cd5b6c7f75c7773c315023096a23447
Emily Hernandez <bulle.96@windowslive.com>
2019-01-07 14:14:20 +0200

    Added Ultrahack in numbers

--- app/containers/PartnerPage/index.js
+++ app/containers/PartnerPage/index.js
@@ -11,24 +11,73 @@ import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
+import { Typography } from '@material-ui/core';
 import makeSelectPartnerPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 
+const styles = {
+  stats: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  statsRow: {
+    display: 'grid',
+    gridTemplate: 'auto / 80px 100px',
+  },
+};
+
 /* eslint-disable react/prefer-stateless-function */
 export class PartnerPage extends React.Component {
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
         <Helmet>
-          <title>PartnerPage</title>
+          <title>Ultrahack | Partners</title>
           <meta name="description" content="Description of PartnerPage" />
         </Helmet>
         <FormattedMessage {...messages.header} />
+        <div className={classes.stats} />
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.community}</Typography>
+          <Typography>community members</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.participants}</Typography>
+          <Typography>selected participants</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.challenges}</Typography>
+          <Typography>challenges</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.solutions}</Typography>
+          <Typography>solutions</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.partners}</Typography>
+          <Typography>industry partners</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.mentors}</Typography>
+          <Typography>mentors</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          {' '}
+          <Typography variant="h5">{messages.stats.volunteers}</Typography>
+          <Typography>volunteers</Typography>
+        </div>
+        <div className={classes.statsRow}>
+          <Typography variant="h5">{messages.stats.organizers}</Typography>
+          <Typography>organizers</Typography>
+        </div>
       </div>
     );
   }
@@ -36,6 +85,7 @@ export class PartnerPage extends React.Component {
 
 PartnerPage.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -57,6 +107,7 @@ const withReducer = injectReducer({ key: 'partnerPage', reducer });
 const withSaga = injectSaga({ key: 'partnerPage', saga });
 
 export default compose(
+  withStyles(styles),
   withReducer,
   withSaga,
   withConnect,
--- app/containers/PartnerPage/messages.js
+++ app/containers/PartnerPage/messages.js
@@ -13,4 +13,14 @@ export default defineMessages({
     id: `${scope}.header`,
     defaultMessage: 'This is the PartnerPage container!',
   },
+  stats: {
+    community: 6000,
+    participants: 1800,
+    challenges: 50,
+    solutions: 200,
+    partners: 100,
+    mentors: 200,
+    volunteers: 50,
+    organizers: 20,
+  },
 });
