db1fb518672006702f6a178d6d3ee59911ee1703
Emily Hernandez <bulle.96@windowslive.com>
2019-01-09 16:11:42 +0200

    Edit About Page

--- app/containers/AboutPage/messages.js
+++ app/containers/AboutPage/messages.js
@@ -13,4 +13,15 @@ export default defineMessages({
     id: `${scope}.header`,
     defaultMessage: 'This is the AboutPage container!',
   },
+  aboutUs: [
+    `Ultrahack combines hackathons, an innovation platform and accelerators in a 
+    unique manner. Our mission is to build a community where the best talent and 
+    leading industry partners could meet and collaborate towards future 
+    innovations.`,
+    `Ultrahack started in 2015 as a joint operation of Digital Forum Finland 
+    Association (before Teleforum ry), Futuretournaments Oy and TKO-äly ry 
+    (The Student Union of the Department of Computer Science in the University 
+      of Helsinki.) Nowadays Ultrahack is operated by Futuretournaments Oy in 
+      co-operation with TKO-äly ry and Digital Forum Finland.`,
+  ],
 });
