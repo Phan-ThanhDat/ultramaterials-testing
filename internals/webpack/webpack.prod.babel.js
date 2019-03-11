c2d92e12ec2a54c042d2e9e9778340d5aff8692a
Sami <sami.haapanen@hotmail.com>
2019-03-11 16:27:24 +0200

    Edit progressive web app manifest

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -121,20 +121,20 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
-      background_color: '#fafafa',
-      theme_color: '#b1624d',
+      name: 'Ultrahack',
+      short_name: 'UH',
+      description: 'Ultrahack React Boilerplate-based project!',
+      background_color: '#00060c',
+      theme_color: '#da0035',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/android-icon-192x192.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/android-icon-192x192.png'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
